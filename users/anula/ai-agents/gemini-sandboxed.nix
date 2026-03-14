d0f6abc4a006e0793850e63a4940fb9f9e1cd7a4
anula <anula@users.noreply.github.com>
2026-03-14 19:13:46 +0100

    Update gemini CLI version

--- users/anula/ai-agents/gemini-sandboxed.nix
+++ users/anula/ai-agents/gemini-sandboxed.nix
@@ -4,19 +4,19 @@ let
 
   gemini-pinned = pkgs.buildNpmPackage rec {
     pname = "gemini-cli";
-    version = "0.33.1";
+    version = "0.38.1";
 
     src = pkgs.fetchFromGitHub {
       owner = "google-gemini";
       repo = "gemini-cli";
       rev = "v${version}";
-      hash = "sha256-dDP+UcIuajyad0tZz6/jqJ9AMERUtyn0z2ohkGiSSj0=";
+      hash = "sha256-Iq/KxQ8rbLtXDbGzcZxspfFwar189H3mBWwOD4hO7HU=";
     };
     npmFlags = [ "--install-links" ];
     makeCacheWritable = true;
     dontCheckForBrokenSymlinks = true;
 
-    npmDepsHash = "sha256-Frne1xZoMqcJowMzhGrBpTYcjqQuUgbP2ak63NYbHlY=";
+    npmDepsHash = "sha256-T3fxNFvkLR7f49GQjzzTnl3VM+VUUgJfFF5d2GGe7L4=";
 
     nativeBuildInputs = [ pkgs.pkg-config ];
     buildInputs = [ pkgs.libsecret ];
