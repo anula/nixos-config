183025e1228d8ab81f21c71dccc03e27e068d28a
anula <anula@users.noreply.github.com>
2026-05-03 18:45:15 +0200

    Update talosctl to 1.13.0

--- users/anula/kubernetes.nix
+++ users/anula/kubernetes.nix
@@ -2,12 +2,12 @@
 
 let
   # Pin Talosctl version here
-  talosVersion = "1.10.5";
+  talosVersion = "1.13.0";
   talosSrc = pkgs.fetchurl {
     url = "https://github.com/siderolabs/talos/releases/download/v${talosVersion}/talosctl-linux-amd64";
     # To get the SHA256:
     # nix-prefetch-url https://github.com/siderolabs/talos/releases/download/v${talosVersion}/talosctl-linux-amd64
-    sha256 = "0svpa119c9cb3zp6lwqf4frba2iz8di7mbg3dqrb27s47q2rnykx";
+    sha256 = "1biv8yld4p4jmbr0fynwsimwrc0zs9yzv2rm7cd8r2vh3hcwv9aa";
   };
 
   talosctl-pinned = pkgs.runCommand "talosctl" { } ''
